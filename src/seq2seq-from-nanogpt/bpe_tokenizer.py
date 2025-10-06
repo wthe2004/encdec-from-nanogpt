e777ef6ee07128c83a0eaec8801971324dabafda
Wentao He <goodmorning_hwt@sjtu.edu.cn>
2025-10-06 14:15:59 -0400

    feat: bpe tokeinizer for seq2seq

--- src/seq2seq-from-nanogpt/bpe_tokenizer.py
+++ src/seq2seq-from-nanogpt/bpe_tokenizer.py
@@ -1,12 +1,23 @@
 import os
+from datasets import load_dataset
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers
 from tokenizers.decoders import ByteLevel as ByteLevelDecoder
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+
+
+def batch_iterator(dataset, batch_size=1000):
+    for i in range(0, len(dataset["train"]), batch_size):
+        batch = dataset["train"][i : i + batch_size]
+        yield [text for text in batch["src"] if text is not None]
+        yield [text for text in batch["ref"] if text is not None]
 
 
 def get_bpe_tokenizer(
-    train_file: str,
+    dataset_name: str,
     vocab_size: int,
     tokenizer_path: str,
+    lp: str = "en-de",
 ) -> Tokenizer:
     """
     Trains a new BPE tokenizer or loads an existing one.
@@ -15,12 +26,17 @@ def get_bpe_tokenizer(
         print(f"Loading existing tokenizer: {tokenizer_path}")
         return Tokenizer.from_file(tokenizer_path)
     else:
-        return train_bpe_tokenizer(train_file, vocab_size, tokenizer_path)
+        return train_bpe_tokenizer(dataset_name, vocab_size, tokenizer_path, lp=lp)
 
 
 def train_bpe_tokenizer(
-    train_file: str, vocab_size: int, tokenizer_path: str
+    dataset_name: str,
+    vocab_size: int,
+    tokenizer_path: str,
+    lp="en-de",
 ) -> Tokenizer:
+    raw_dataset = load_dataset(dataset_name)
+    dataset = raw_dataset.filter(lambda x: x["lp"] == lp)
     print("Training a new BPE tokenizer...")
     tokenizer = Tokenizer(models.BPE(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
@@ -30,19 +46,21 @@ def train_bpe_tokenizer(
         vocab_size=vocab_size,
         special_tokens=["[UNK]", "[PAD]", "[CLS]", "[SEP]", "[MASK]"],
     )
-    tokenizer.train([train_file], trainer)
+
+    tokenizer.train_from_iterator(batch_iterator(dataset=dataset), trainer=trainer)
+
     tokenizer.save(tokenizer_path)
     print(f"Tokenizer trained and saved to: {tokenizer_path}")
     return tokenizer
 
 
 if __name__ == "__main__":
-    # Example usage
-    vocab_size = 5000
-    train_file = "./shakespeare.txt"
-    tokenizer_path = f"bpe_shakespeare_vocab{vocab_size}.json"
-    tokenizer = train_bpe_tokenizer(train_file, vocab_size, tokenizer_path)
 
-    print(f"Tokenizer vocabulary size: {tokenizer.get_vocab_size()}")
-    # save
-    tokenizer.save(tokenizer_path)
+    tokenizer = get_bpe_tokenizer(
+        dataset_name="IWSLT/da2023",
+        vocab_size=300,
+        tokenizer_path="bpe_iwslt2023_vocab300.json",
+        lp="en-de",
+    )
+
+    print(tokenizer)
